d66ce8f0a80d5616ebe524fe488a4d24cb770899
kavya <akkiraju.saikavya@gmail.com>
2022-03-15 15:27:23 +0530

    https://github.com/the-10x-academy/JavaScript-Assignments

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,21 @@
 // This function should return the BMI for a person 
-function BMICalculator(mass, height) {
+function BMICalculator(mass,name, height) {
     // Write your code here
+        if(!((typeof(height)==='number')&&(typeof(mass)==='number'))){
+            console.log("INVALID INPUTS");
+        }
+        let heightInMet=height/100;
+        let bmi=mass/Math.pow(heightInMet,2);
+        if(bmi<18){
+            console.log(name+",is underweight");
+        }else if(bmi>18 && bmi<=24.9){
+            console.log(name+",is overweight");
+        }else if(bmi>25){
+            console.log(name+",is overweight");
+        }else if(bmi>=30){
+            console.log(name+",is obese");
+        }
 }
+BMICalculator(18,'kavuu',70)
 
 module.exports = BMICalculator;
--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,13 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(!hasDrivingLiscence){
+        return "you cannot drive";
+    } else {
+        if(isTired || !isSober){
+            return "you sholdnt drive";
+           }
+    }
+    return "you can drive";
 }
-
+CanDrive(hasDrivingLiscence,isTired,isSober)
 module.exports = CanDrive;
